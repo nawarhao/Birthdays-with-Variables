73b3d763ecb63a88a249f1bb3e560ee0a9972ae3
razonno <nrazonno@gmail.com>
2025-04-19 12:28:35 -0400

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,31 @@
+#Input statements for birthdays
+b1= input("When is your birthday? ")
+b2= input("When is Nubah's birthday? ")
+b3= input("When is Raone's birthday? ")
+b4= input("When is mom's brithday? ")
+b5= input("When is dad's birthday? ")
+
+if b1 >= "May 26":
+    print(f"\n Razonno's birthday is", b1) 
+else: 
+    print("\n You are incorrect. Haha")
+
+if b2 >= "January 11":
+    print(f"\n Nubah's birthday is indeed", b2)
+else: 
+    print("\n You are absolutely wrong")
+
+if b3 >= "February 26":
+    print(f"\n Raone's birthday is indeed", b3)
+else:
+    print("\n You are incorrect. Bruh")
+
+if b4 >= "December 8":
+    print(f"\n Mom's birthday is indeed", b4)
+else:
+    print("\n You are incorrect")
+
+if b5 >= "November 27":
+    print(f"\n Dad's birthday is indeed", b5)
+else:
+    print("\n You are incorrect")
